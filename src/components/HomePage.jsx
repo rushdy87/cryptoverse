490b26e07bf93468287ecca93fca72adc90230d0
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-01-24 12:55:14 +0300

    Home Page: Static

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,34 @@
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Title level={2} className="haeding">
+        Global Crepto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Creptocurrencies" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Valume" value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={5} />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default HomePage;
